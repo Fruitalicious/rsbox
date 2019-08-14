aded8146dacbda93bc042fa07f063419daf1056e
BuildTools <unconfigured@null.spigotmc.org>
2019-08-13 20:55:51 -0700

    Updated Varbits

--- api/src/main/kotlin/io/rsbox/api/consts/Varbit.kt
+++ api/src/main/kotlin/io/rsbox/api/consts/Varbit.kt
@@ -37,4 +37,447 @@ object Varbit {
     const val PRIORITY_PID_VARP = 1075
     const val SELECTED_AUTOCAST_VARBIT = 276
     const val DEFENSIVE_MAGIC_CAST_VARBIT = 2668
+
+    /**
+     * If the player has an active stamina potion effect or not
+     */
+    const val RUN_SLOWED_DEPLETION_ACTIVE = 25
+    /**
+     * Runepouch
+     */
+    const val RUNE_POUCH_RUNE1 = 29
+    const val RUNE_POUCH_RUNE2 = 1622
+    const val RUNE_POUCH_RUNE3 = 1623
+    const val RUNE_POUCH_AMOUNT1 = 1624
+    const val RUNE_POUCH_AMOUNT2 = 1625
+    const val RUNE_POUCH_AMOUNT3 = 1626
+
+    /**
+     * Prayers
+     */
+    const val QUICK_PRAYER = 4103
+    const val PRAYER_THICK_SKIN = 4104
+    const val PRAYER_BURST_OF_STRENGTH = 4105
+    const val PRAYER_CLARITY_OF_THOUGHT = 4106
+    const val PRAYER_SHARP_EYE = 4122
+    const val PRAYER_MYSTIC_WILL = 4123
+    const val PRAYER_ROCK_SKIN = 4107
+    const val PRAYER_SUPERHUMAN_STRENGTH = 4108
+    const val PRAYER_IMPROVED_REFLEXES = 4109
+    const val PRAYER_RAPID_RESTORE = 4110
+    const val PRAYER_RAPID_HEAL = 4111
+    const val PRAYER_PROTECT_ITEM = 4112
+    const val PRAYER_HAWK_EYE = 4124
+    const val PRAYER_MYSTIC_LORE = 4125
+    const val PRAYER_STEEL_SKIN = 4113
+    const val PRAYER_ULTIMATE_STRENGTH = 4114
+    const val PRAYER_INCREDIBLE_REFLEXES = 4115
+    const val PRAYER_PROTECT_FROM_MAGIC = 4116
+    const val PRAYER_PROTECT_FROM_MISSILES = 4117
+    const val PRAYER_PROTECT_FROM_MELEE = 4118
+    const val PRAYER_EAGLE_EYE = 4126
+    const val PRAYER_MYSTIC_MIGHT = 4127
+    const val PRAYER_RETRIBUTION = 4119
+    const val PRAYER_REDEMPTION = 4120
+    const val PRAYER_SMITE = 4121
+    const val PRAYER_CHIVALRY = 4128
+    const val PRAYER_PIETY = 4129
+    const val PRAYER_PRESERVE = 5466
+    const val PRAYER_RIGOUR = 5464
+    const val PRAYER_AUGURY = 5465
+
+    /**
+     * Diary Entries
+     */
+    const val DIARY_ARDOUGNE_EASY = 4458
+    const val DIARY_ARDOUGNE_MEDIUM = 4459
+    const val DIARY_ARDOUGNE_HARD = 4460
+    const val DIARY_ARDOUGNE_ELITE = 4461
+
+    const val DIARY_DESERT_EASY = 4483
+    const val DIARY_DESERT_MEDIUM = 4484
+    const val DIARY_DESERT_HARD = 4485
+    const val DIARY_DESERT_ELITE = 4486
+
+    const val DIARY_FALADOR_EASY = 4462
+    const val DIARY_FALADOR_MEDIUM = 4463
+    const val DIARY_FALADOR_HARD = 4464
+    const val DIARY_FALADOR_ELITE = 4465
+
+    const val DIARY_FREMENNIK_EASY = 4491
+    const val DIARY_FREMENNIK_MEDIUM = 4492
+    const val DIARY_FREMENNIK_HARD = 4493
+    const val DIARY_FREMENNIK_ELITE = 4494
+
+    const val DIARY_KANDARIN_EASY = 4475
+    const val DIARY_KANDARIN_MEDIUM = 4476
+    const val DIARY_KANDARIN_HARD = 4477
+    const val DIARY_KANDARIN_ELITE = 4478
+
+    const val DIARY_KARAMJA_EASY = 3578
+    const val DIARY_KARAMJA_MEDIUM = 3599
+    const val DIARY_KARAMJA_HARD = 3611
+
+    const val DIARY_KOUREND_EASY = 7925
+    const val DIARY_KOUREND_MEDIUM = 7926
+    const val DIARY_KOUREND_HARD = 7927
+    const val DIARY_KOUREND_ELITE = 7928
+
+    const val DIARY_LUMBRIDGE_EASY = 4495
+    const val DIARY_LUMBRIDGE_MEDIUM = 4496
+    const val DIARY_LUMBRIDGE_HARD = 4497
+    const val DIARY_LUMBRIDGE_ELITE = 4498
+
+    const val DIARY_MORYTANIA_EASY = 4487
+    const val DIARY_MORYTANIA_MEDIUM = 4488
+    const val DIARY_MORYTANIA_HARD = 4489
+    const val DIARY_MORYTANIA_ELITE = 4490
+
+    const val DIARY_VARROCK_EASY = 4479
+    const val DIARY_VARROCK_MEDIUM = 4480
+    const val DIARY_VARROCK_HARD = 4481
+    const val DIARY_VARROCK_ELITE = 4482
+
+    const val DIARY_WESTERN_EASY = 4471
+    const val DIARY_WESTERN_MEDIUM = 4472
+    const val DIARY_WESTERN_HARD = 4473
+    const val DIARY_WESTERN_ELITE = 4474
+
+    const val DIARY_WILDERNESS_EASY = 4466
+    const val DIARY_WILDERNESS_MEDIUM = 4467
+    const val DIARY_WILDERNESS_HARD = 4468
+    const val DIARY_WILDERNESS_ELITE = 4469
+
+    /**
+     * Kourend house favours
+     */
+    const val KOUREND_FAVOR_ARCEUUS = 4896
+    const val KOUREND_FAVOR_HOSIDIUS = 4895
+    const val KOUREND_FAVOR_LOVAKENGJ = 4898
+    const val KOUREND_FAVOR_PISCARILIUS = 4899
+    const val KOUREND_FAVOR_SHAYZIEN = 4894
+
+    /**
+     * Equipped weapon type
+     */
+    const val EQUIPPED_WEAPON_TYPE = 357
+
+    /**
+     * Herbiboar Trails
+     */
+    const val HB_TRAIL_31303 = 5737
+    const val HB_TRAIL_31306 = 5738
+    const val HB_TRAIL_31309 = 5739
+    const val HB_TRAIL_31312 = 5740
+    const val HB_TRAIL_31315 = 5741
+    const val HB_TRAIL_31318 = 5742
+    const val HB_TRAIL_31321 = 5743
+    const val HB_TRAIL_31324 = 5744
+    const val HB_TRAIL_31327 = 5745
+    const val HB_TRAIL_31330 = 5746
+
+    const val HB_TRAIL_31333 = 5768
+    const val HB_TRAIL_31336 = 5769
+    const val HB_TRAIL_31339 = 5770
+    const val HB_TRAIL_31342 = 5771
+    const val HB_TRAIL_31345 = 5772
+    const val HB_TRAIL_31348 = 5773
+    const val HB_TRAIL_31351 = 5774
+    const val HB_TRAIL_31354 = 5775
+    const val HB_TRAIL_31357 = 5776
+    const val HB_TRAIL_31360 = 5777
+
+    const val HB_TRAIL_31363 = 5747
+    const val HB_TRAIL_31366 = 5748
+    const val HB_TRAIL_31369 = 5749
+    const val HB_TRAIL_31372 = 5750
+
+    const val HB_FINISH = 5766
+    const val HB_STARTED = 5767 //not working
+
+    /**
+     * Barbarian Assault
+     */
+    const val IN_GAME_BA = 3923
+    const val BA_GC = 4768
+
+    /**
+     * 0 = Outside wilderness
+     * 1 = In wilderness
+     */
+    const val IN_WILDERNESS = 5963
+
+    /**
+     * Fishing Trawler
+     * FISHING_TRAWLER_ACTIVITY Expected values: 0-255
+     */
+    const val FISHING_TRAWLER_ACTIVITY = 3377
+
+    /**
+     * Blast Furnace Bar Dispenser
+     *
+     * These are the expected values:
+     *	0 = No bars being processed
+     *	1 = Ores are being processed on the conveyor belt, bar dispenser cannot be checked
+     *	2 = Bars are cooling down
+     *	3 = Bars can be collected
+     */
+    const val BAR_DISPENSER = 936
+
+    /**
+     * Motherlode mine sack
+     */
+    const val SACK_NUMBER = 5558
+    const val SACK_UPGRADED = 5556
+
+    /**
+     * Experience tracker
+     *
+     * EXPERIENCE_TRACKER_POSITION expected values:
+     *   0 = Right
+     *   1 = Middle
+     *   2 = Left
+     */
+    const val EXPERIENCE_TRACKER_POSITION = 4692
+    const val EXPERIENCE_TRACKER_COUNTER = 4697
+    const val EXPERIENCE_TRACKER_PROGRESS_BAR = 4698
+
+    /**
+     * Experience drop color
+     */
+    const val EXPERIENCE_DROP_COLOR = 4695
+
+    /**
+     * Tithe Farm
+     */
+    const val TITHE_FARM_SACK_AMOUNT = 4900
+    const val TITHE_FARM_SACK_ICON = 5370
+    const val TITHE_FARM_POINTS = 4893
+
+    /**
+     * Blast Mine
+     */
+    const val BLAST_MINE_COAL = 4924
+    const val BLAST_MINE_GOLD = 4925
+    const val BLAST_MINE_MITHRIL = 4926
+    const val BLAST_MINE_ADAMANTITE = 4921
+    const val BLAST_MINE_RUNITE = 4922
+
+    /**
+     * Raids
+     */
+    const val IN_RAID = 5432
+    const val TOTAL_POINTS = 5431
+    const val PERSONAL_POINTS = 5422
+    const val RAID_PARTY_SIZE = 5424
+
+    /**
+     * Theatre of Blood 1=In Party, 2=Inside/Spectator, 3=Dead Spectating
+     */
+    const val THEATRE_OF_BLOOD = 6440
+
+    /**
+     * Nightmare Zone
+     */
+    const val NMZ_ABSORPTION = 3956
+    const val NMZ_POINTS = 3949
+
+    /**
+     * Blast Furnace
+     */
+    const val BLAST_FURNACE_COPPER_ORE = 959
+    const val BLAST_FURNACE_TIN_ORE = 950
+    const val BLAST_FURNACE_IRON_ORE = 951
+    const val BLAST_FURNACE_COAL = 949
+    const val BLAST_FURNACE_MITHRIL_ORE = 952
+    const val BLAST_FURNACE_ADAMANTITE_ORE = 953
+    const val BLAST_FURNACE_RUNITE_ORE = 954
+    const val BLAST_FURNACE_SILVER_ORE = 956
+    const val BLAST_FURNACE_GOLD_ORE = 955
+
+    const val BLAST_FURNACE_BRONZE_BAR = 941
+    const val BLAST_FURNACE_IRON_BAR = 942
+    const val BLAST_FURNACE_STEEL_BAR = 943
+    const val BLAST_FURNACE_MITHRIL_BAR = 944
+    const val BLAST_FURNACE_ADAMANTITE_BAR = 945
+    const val BLAST_FURNACE_RUNITE_BAR = 946
+    const val BLAST_FURNACE_SILVER_BAR = 948
+    const val BLAST_FURNACE_GOLD_BAR = 947
+
+    const val BLAST_FURNACE_COFFER = 5357
+
+    /**
+     * Pyramid plunder
+     */
+    const val PYRAMID_PLUNDER_TIMER = 2375
+    const val PYRAMID_PLUNDER_ROOM = 2377
+
+    /**
+     * Barrows
+     */
+    const val BARROWS_KILLED_AHRIM = 457
+    const val BARROWS_KILLED_DHAROK = 458
+    const val BARROWS_KILLED_GUTHAN = 459
+    const val BARROWS_KILLED_KARIL = 460
+    const val BARROWS_KILLED_TORAG = 461
+    const val BARROWS_KILLED_VERAC = 462
+    const val BARROWS_REWARD_POTENTIAL = 463
+    const val BARROWS_NPCS_SLAIN = 464
+
+    /**
+     * Spicy stew ingredients
+     */
+    const val SPICY_STEW_RED_SPICES = 1879
+    const val SPICY_STEW_YELLOW_SPICES = 1880
+    const val SPICY_STEW_BROWN_SPICES = 1881
+    const val SPICY_STEW_ORANGE_SPICES = 1882
+
+    /**
+     * Multicombat area
+     */
+    const val MULTICOMBAT_AREA = 4605
+
+    /**
+     * Kingdom Management
+     */
+    const val KINGDOM_FAVOR = 72
+    const val KINGDOM_COFFER = 74
+
+    /**
+     * The Hand in the Sand quest status
+     */
+    const val QUEST_THE_HAND_IN_THE_SAND = 1527
+
+    /**
+     * Daily Tasks  = Collection availability)
+     */
+    const val DAILY_HERB_BOXES_COLLECTED = 3961
+    const val DAILY_STAVES_COLLECTED = 4539
+    const val DAILY_ESSENCE_COLLECTED = 4547
+    const val DAILY_RUNES_COLLECTED = 4540
+    const val DAILY_SAND_COLLECTED = 4549
+    const val DAILY_FLAX_STATE = 4559
+    const val DAILY_ARROWS_STATE = 4563
+
+    /**
+     * This varbit tracks how much bonemeal has been redeemed from Robin
+     * The player gets 13 for each diary completed above and including Medium, for a maxiumum of 39
+     * */
+    const val DAILY_BONEMEAL_STATE = 4543
+
+    const val DAILY_DYNAMITE_COLLECTED = 7939
+
+    /**
+     * Fairy Ring
+     */
+    const val FAIR_RING_LAST_DESTINATION = 5374
+    const val FAIRY_RING_DIAL_ADCB = 3985 //Left dial
+    const val FAIRY_RIGH_DIAL_ILJK = 3986 //Middle dial
+    const val FAIRY_RING_DIAL_PSRQ = 3987 //Right dial
+
+    /**
+     * Transmog controllers for farming
+     */
+    const val FARMING_4771 = 4771
+    const val FARMING_4772 = 4772
+    const val FARMING_4773 = 4773
+    const val FARMING_4774 = 4774
+    const val FARMING_4775 = 4775
+    const val FARMING_7904 = 7904
+    const val FARMING_7905 = 7905
+    const val FARMING_7906 = 7906
+    const val FARMING_7907 = 7907
+    const val FARMING_7908 = 7908
+    const val FARMING_7909 = 7909
+    const val FARMING_7910 = 7910
+    const val FARMING_7911 = 7911
+
+    /**
+     * Transmog controllers for grapes
+     */
+    const val GRAPES_4953 = 4953
+    const val GRAPES_4954 = 4954
+    const val GRAPES_4955 = 4955
+    const val GRAPES_4956 = 4956
+    const val GRAPES_4957 = 4957
+    const val GRAPES_4958 = 4958
+    const val GRAPES_4959 = 4959
+    const val GRAPES_4960 = 4960
+    const val GRAPES_4961 = 4961
+    const val GRAPES_4962 = 4962
+    const val GRAPES_4963 = 4963
+    const val GRAPES_4964 = 4964
+
+    /**
+     * Automatically weed farming patches
+     */
+    const val AUTOWEED = 5557
+
+    /**
+     * The varbit that stores the players {@code AccountType}.
+     */
+    const val ACCOUNT_TYPE = 1777
+
+    /**
+     * The varbit that stores the oxygen percentage for player
+     */
+    const val OXYGEN_LEVEL = 5811
+
+    /**
+     * Corp beast damage
+     */
+    const val CORP_DAMAGE = 999
+
+    /**
+     * Toggleable slayer unlocks
+     */
+    const val SUPERIOR_ENABLED = 5362
+    const val FOSSIL_ISLAND_WYVERN_DISABLE = 6251
+
+    const val CURRENT_BANK_TAB = 4150
+
+    const val WORLDHOPPER_FAVROITE_1 = 4597
+    const val WORLDHOPPER_FAVROITE_2 = 4598
+
+    /**
+     * Vengeance is active
+     */
+    const val VENGEANCE_ACTIVE = 2450
+
+    /**
+     * Spell cooldowns
+     */
+    const val VENGEANCE_COOLDOWN = 2451
+
+    /**
+     * Amount of items in each bank tab
+     */
+    const val BANK_TAB_ONE_COUNT = 4171
+    const val BANK_TAB_TWO_COUNT = 4172
+    const val BANK_TAB_THREE_COUNT = 4173
+    const val BANK_TAB_FOUR_COUNT = 4174
+    const val BANK_TAB_FIVE_COUNT = 4175
+    const val BANK_TAB_SIX_COUNT = 4176
+    const val BANK_TAB_SEVEN_COUNT = 4177
+    const val BANK_TAB_EIGHT_COUNT = 4178
+    const val BANK_TAB_NINE_COUNT = 4179
+
+    /**
+     * Type of GE offer currently being created
+     * 0 = buy
+     * 1 = sell
+     */
+    const val GE_OFFER_CREATION_TYPE = 4397
+
+    /**
+     * Explorer ring
+     */
+    const val EXPLORER_RING_ALCHTYPE = 5398
+    const val EXPLORER_RING_TELEPORTS = 4552
+    const val EXPLORER_RING_ALCHS = 4554
+    const val EXPLORER_RING_RUNENERGY = 4553
+
+    const val WINTERTODT_TIMER = 7980
+
+
 }
