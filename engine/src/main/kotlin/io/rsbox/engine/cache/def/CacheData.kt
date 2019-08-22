8ced0df1f5ee41e101085903d26d9a2622194e30
kyle-escobar <kslrtips@gmail.com>
2019-08-21 22:26:58 -0400

    Im tired. Off to bed.

--- engine/src/main/kotlin/io/rsbox/engine/cache/def/CacheData.kt
+++ engine/src/main/kotlin/io/rsbox/engine/cache/def/CacheData.kt
@@ -3,12 +3,18 @@ package io.rsbox.engine.cache.def
 import io.netty.buffer.Unpooled
 import io.rsbox.engine.Engine
 import io.rsbox.engine.cache.def.impl.*
+import io.rsbox.engine.model.world.ChunkSet
+import io.rsbox.engine.model.world.Tile
+import io.rsbox.engine.model.world.World
+import io.rsbox.engine.service.ServiceManager
 import io.rsbox.engine.service.impl.XteaKeyService
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
 import mu.KLogging
 import net.runelite.cache.ConfigType
 import net.runelite.cache.IndexType
+import net.runelite.cache.definitions.loaders.MapLoader
+import net.runelite.cache.region.Region
 import java.io.FileNotFoundException
 import java.lang.IllegalArgumentException
 
@@ -19,7 +25,7 @@ import java.lang.IllegalArgumentException
 class CacheData : io.rsbox.api.cache.CacheData {
     private val defs = Object2ObjectOpenHashMap<Class<out CacheDef>, Map<Int, *>>()
 
-    private var xteaKeyService: XteaKeyService? = null
+    private var xteaKeyService: XteaKeyService = ServiceManager[XteaKeyService::class.java]!!
 
     private val store = Engine.cacheStore
 
@@ -46,7 +52,18 @@ class CacheData : io.rsbox.api.cache.CacheData {
         logger.info("Loaded ${getCount(CacheVarp::class.java)} varp defs.")
     }
 
-    fun loadRegions() {}
+    fun loadRegions(world: World, chunks: ChunkSet, regions: IntArray) {
+        var loaded = 0
+        regions.forEach { region ->
+            if(chunks.activeRegions.add(region)) {
+                if(createRegion(world, region)) {
+                    loaded++
+                }
+            }
+        }
+
+        logger.info("Loaded $loaded world regions.")
+    }
 
     fun <T : CacheDef> load(type: Class<out T>) {
         val configType = this.classToType(type) ?: throw Exception("Unknown cache type class ${type.simpleName}.")
@@ -106,6 +123,36 @@ class CacheData : io.rsbox.api.cache.CacheData {
             CacheVarp::class.java -> ConfigType.VARPLAYER
             else -> null
         }
+    }
+
+    fun createRegion(world: World, regionId: Int): Boolean {
+        val regionIndex = store.getIndex(IndexType.MAPS)
+
+        val x = regionId shr 8
+        val z = regionId and 0xFF
+
+        val mapArchive = regionIndex.findArchiveByName("m${x}_$z") ?: return false
+        val landArchive = regionIndex.findArchiveByName("l${x}_$z") ?: return false
+        val mapData = mapArchive.decompress(store.storage.loadArchive(mapArchive))
+
+        if(mapData == null) {
+            logger.error("Unable to load map data for region $regionId.")
+            return false
+        }
+
+        val mapDef = MapLoader().load(x, z, mapData)
+
+        val cacheRegion = Region(regionId)
+        cacheRegion.loadTerrain(mapDef)
+
+        /**
+         * Collision stuff
+         */
+        val blocked = hashSetOf<Tile>()
+        val bridges = hashSetOf<Tile>()
+
+
+        return true
     }
 
     companion object : KLogging()
